b2d0ecb8055c9092742da96a8e68db980350ec74
Denis <denisfolyush@gmail.com>
2024-03-14 21:45:37 +0200

    add lab 3 varianr 3

---
+++ lab3_V3/test/unittest_lab3_V3.py
@@ -0,0 +1,15 @@
+import unittest
+
+from ..str.lab3_V3 import TreeNode,is_tree_balanced
+
+class TestBinaryTree(unittest.TestCase):
+    def test_is_tree_balanced(self):
+        root = TreeNode(3)
+        root.left = TreeNode(9)
+        root.right = TreeNode(20)
+        root.right.left = TreeNode(15)
+        root.right.right = TreeNode(7)
+        self.assertFalse(is_tree_balanced(root))
+
+if __name__ == '__main__':
+    unittest.main()
