84dee4ca8f1d10be7af6caeea7146f4a644d0c56
Denis <denisfolyush@gmail.com>
2024-04-12 11:02:16 +0300

    add src & test and first lab

--- test/Sliding_windiw_unittest.py
+++ test/Sliding_windiw_unittest.py
@@ -1,11 +1,11 @@
 import unittest
 
-from labka1_v3 import sorting
+from src.Sliding_windiw import sorting
 
 
 class TestSorting(unittest.TestCase):
     def test_sorting(self):
-        self.assertEqual(sorting([1, 2, 4, 7, 10, 11, 7, 12, 6, 7, 16, 18, 19]), (3, 9))
+        self.assertEqual(sorting([1, 2, 4, 7, 10, 11, 7, 12, 6, 7, 16, 18, 19]), (3, 8))
         self.assertEqual(sorting([1, 2, 3, 4, 5]), (-1, -1))
         self.assertEqual(sorting([1]), (-1, -1))
 
